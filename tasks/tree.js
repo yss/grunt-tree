121471caf716acc161a753230b9b751bc48226e7
yansong <yss.nelson@gmail.com>
2013-03-20 11:34:48 +0800

    Implements the main function

--- tasks/tree.js
+++ tasks/tree.js
@@ -1,6 +1,6 @@
 /*
  * grunt-tree
- * https://github.com/yss/grunt-contrib-tree
+ * https://github.com/yss/grunt-tree
  *
  * Copyright (c) 2013 yansong
  * Licensed under the MIT license.
@@ -8,43 +8,107 @@
 
 'use strict';
 
+var path = require('path'),
+    fs = require('fs'),
+    crypto = require('crypto');
+
 module.exports = function(grunt) {
+    grunt.registerMultiTask('tree', 'Parse a directory to a tree with json format.', function() {
+        var options = this.options({
+            recursion: true,
+            type: [],
+            md5: false,
+            format: false
+        });
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+        this.files.forEach(function(f) {
+            var tree = {};
+            f.src.filter(function(filepath) {
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).forEach(function(filepath) {
+                grunt.file.recurse(filepath, function(abspath, rootdir, subdir, filename) {
+                    grunt.log.writeln(filename);
+                    // ignore hidden file
+                    if (filename.indexOf('.') === 0) {
+                        return;
+                    }
+                    if (grunt.file.isFile(abspath)) {
+                        tree[getFileName(filename)] = path.join(subdir, getMd5Name(abspath, filename, options.md5));
+                    }
+                });
+            });
 
-  grunt.registerMultiTask('tree', 'Your task description goes here.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+
+            console.log(tree);
+            if (!options.format) {
+                tree = parseToTree(tree, options.md5);
+            }
+            console.log(tree);
+
+            grunt.file.write(f.dest, JSON.stringify(tree));
+            grunt.log.writeln('File "' + f.dest + '" created.');
+        });
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+};
 
-      // Handle options.
-      src += options.punctuation;
+/**
+ * parse a path object to a tree object
+ */
+function parseToTree(obj, md5) {
+    var key, i, arr, len, tmp,
+        tree = {};
+    for (key in obj) {
+        tmp = tree;
+        obj[key].split(path.sep).forEach(function(item, i, arr) {
+            // last
+            if (i === arr.length - 1) {
+                var fileArr = item.split('.');
+                if (md5) {
+                    fileArr.pop();
+                    if (fileArr.length > 1) {
+                        fileArr.pop();
+                    }
+                }
+                // get the origin value
+                tmp[fileArr.join('.')] = arr.join(path.sep);
+            } else {
+                tmp = tmp[item] = {};
+            }
+        });
+    }
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+    return tree;
+}
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
+// file with lost postfix
+function getFileName(filename, version) {
+    var pos = filename.lastIndexOf('.');
+    if (version) {
+        version = '.' + version;
+        return ~pos ? filename.substring(0, pos) + version + filename.slice(pos) : filename + version;
+    } else {
+        return ~pos ? filename.substring(0, pos) : filename;
+    }
+};
 
+function getMd5Name(abspath, filename, md5) {
+    if (md5) {
+        var len = typeof md5 === 'number' ? md5 : 0,
+            version = md5(fs.readFileSync(abspath), '', len);
+        return getFileName(filename, version);
+    }
+    return filename;
+};
+
+// get md5 version of file
+function md5(str, encoding, len) {
+    str = str || Math.random().toString();
+    str = crypto.createHash('md5').update(str).digest(encoding || 'hex');
+    return len ? str.slice(0, len) : str;
 };
